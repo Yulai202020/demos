d394c7ad40b0c16e8b5e25278b55c23ee9497dba
Yulai202020 <ngmyulai@gmail.com>
2024-12-05 17:02:07 +0400

    first commit

--- make.sh
+++ make.sh
@@ -1,7 +1,20 @@
+mkdir $1
+
+cd $1
+
+bun init
+
+bun add -D stylelint
+bun add -D stylelint-config-clean-order
+bun add -D stylelint-config-standard
+
+
+cat <<EOF > .stylelintrc.json
 {
     "extends": ["stylelint-config-standard", "stylelint-config-clean-order"],
     "ignoreFiles": ["node_modules/**/*", "dist.css"],
     "rules": {
       "media-feature-range-notation": "context"
     }
-}  
+}
+EOF
