a7c1ed3c106bceeb56a52e99f8a49a9672247392
Yulai202020 <ngmyulai@gmail.com>
2024-12-18 20:37:13 +0400

    first commit

--- form/src/script.js
+++ form/src/script.js
@@ -4,6 +4,9 @@ function onChange(e, type) {
     let id = type === "p" ? "parent" : "move";
     const moving = document.getElementById(id);
 
+    const border_color = document.getElementById("border-color-"+id).value;
+    const color = document.getElementById("color-"+id).value;
+    const background_color = document.getElementById("background-color-"+id).value;
     const unit = document.getElementById("unit-"+id).value;
     const position = document.getElementById("position-"+id).value;
 
@@ -12,5 +15,5 @@ function onChange(e, type) {
     const right = document.getElementById("right-"+id).value + unit;
     const left = document.getElementById("left-"+id).value + unit;
 
-    moving.setAttribute("style", `position: ${position}; ${top !== unit && `top: ${top}`}; ${bottom !== unit && `bottom: ${bottom}`}; ${right !== unit && `right: ${right}`}; ${left !== unit && `left: ${left}`};`);
+    moving.setAttribute("style", `position: ${position}; ${top !== unit && `top: ${top}`}; ${bottom !== unit && `bottom: ${bottom}`}; ${right !== unit && `right: ${right}`}; ${left !== unit && `left: ${left}`}; color: ${color}; border: 1px solid ${border_color}; background-color: ${background_color}`);
 }
